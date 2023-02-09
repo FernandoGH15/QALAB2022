53bcdeed3c5c299096cae9c435e1ab94480fec4a
FernandoG15 <ofernandogh15@gmail.com>
2023-02-09 14:22:01 -0500

    Agregado comentario Ln3

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Editado por Fernando Gutierrez
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
